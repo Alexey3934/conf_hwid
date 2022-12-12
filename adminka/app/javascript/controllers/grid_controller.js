a4a68b8465b7f1fdc046d4f41974fec4f9397100
Alexey3934 <gorrus39@mial.ru>
2022-12-12 19:05:50 +0100

    part

--- adminka/app/javascript/controllers/grid_controller.js
+++ adminka/app/javascript/controllers/grid_controller.js
@@ -3,16 +3,40 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     gridRowListener()
+    deleteListener()
   }
 }
 
 
-function toggleToDelete(event){
-    event.target.parentNode.classList.toggle('to-delete')
+
+function deleteListener(){
+    const delete_button = $("#delete")
+    delete_button.on('click', ()=>{
+        if (delete_button.hasClass('unactive')) return
+
+        const ids = []
+        $(".grid-user").each((_i,val)=>{
+            if (val.classList.contains('to-delete')) ids.push(val.dataset.id)
+        })
+    })
 }
 
 
 
+
+
+function toggleToDelete(event){
+    event.target.parentNode.classList.toggle('to-delete')
+
+    const delete_button = $('#delete') 
+    let counter = 0
+    $(".grid-user").each((_i,val)=>{
+         if (val.classList.contains('to-delete')) counter+=1
+    })
+    if (counter > 0) delete_button.removeClass('unactive')
+    else delete_button.addClass('unactive')
+}
+
 function gridRowListener() {
-    ($(".grid-user")).each((_i, row)=>row.addEventListener('click', toggleToDelete))    
+    $(".grid-user").each((_i, row)=>row.addEventListener('click', toggleToDelete))    
 }
